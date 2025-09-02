e27ea5e5af6ba1d5ca3dab2dd6db784da8181ad8
Miranda-Dev <joaovictor10_miranda@hotmail.com>
2025-09-02 15:01:58 -0400

    aula 02-09-25
    tudo que escrevi na aula da suzano de conhecendo a linguagem python.

---
+++ conhecendo_a_linguagem/funcoes_entrada_e_saida.py
@@ -0,0 +1,6 @@
+nome = input("Informe seu nome: ")
+idade = input("Informe sua idade: ")
+
+print(f"Ola {nome}, seja muito bem vindo")
+print(nome, end="...\n")
+print(nome, idade, sep="#")
---
+++ conhecendo_a_linguagem/variaveis_constantes.py
@@ -0,0 +1,9 @@
+nome = 'joão'
+
+idade = 22
+
+print(f"me chamo {nome} e tenho {idade} anos.")
+
+VALOR_PI = 3.14
+
+print(f"O valor de pi eh {VALOR_PI}!")
