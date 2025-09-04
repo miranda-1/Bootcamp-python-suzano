024b935a1fb86b2b93014aadb04f74894f49f6a6
Miranda-Dev <joaovictor10_miranda@hotmail.com>
2025-09-04 00:14:45 -0400

    Update Operadores_de_comparacao.py

---
+++ tipos_de_operadores/Operadores_de_comparacao.py
@@ -0,0 +1,10 @@
+saldo_1 = 200
+saldo_2 = 300
+
+
+print(saldo_1 == saldo_2)
+print(saldo_1 != saldo_2)
+print(saldo_1 > saldo_2)
+print(saldo_1 >= saldo_2)
+print(saldo_1 < saldo_2)
+print(saldo_1 <= saldo_2)
